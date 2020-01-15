40d0ac583d7ee7202ce90ad13e00316b9f69a7bd
github username <github email address>
2020-01-15 12:16:58 -0500

    saving work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,33 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    get '/team/:id' do
+      @team = Team.find(params[:id])
+      erb :team
+    end
+
+    get '/super_hero/:id' do
+      @hero = Hero.find(params[:id])
+      erb :super_hero
+    end
+
+    get '/teams' do
+      @teams = Team.all
+      erb :teams
+    end
+
+    post '/teams' do
+      team = params[:team]
+      team[:heroes].each { |hero_data|
+        hero = Hero.new(hero_data)
+        hero.team = @team
+        hero.save
+      }
+      
+    end
 
 end
---
+++ db/migrate/_change_heroes_table_name.rb
@@ -0,0 +1,5 @@
+class ChangeHeroesTableName < ActiveRecord::Migration[5.2]
+    def change
+      rename_table :heroes, :heros
+    end
+end
