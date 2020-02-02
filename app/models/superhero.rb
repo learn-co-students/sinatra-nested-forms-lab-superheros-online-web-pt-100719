64ef7bae24f70598b853fa8a2f10825ee973797e
Justin Miller <jayman1294@gmail.com>
2020-02-02 07:10:18 +0000

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,16 @@
+class SuperHero
+  attr_reader :name, :power, :bio
+  SUPERHEROS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHEROS << self
+  end
+
+ 
+  def self.all
+    SUPERHEROS
+  end
+end
